561b19ccde3279d6484bb67f985f261ea4687032
andres <andres.delgado@talentpitch.co>
2023-05-16 22:21:33 -0500

    create getters and actions for get current location

--- src/store/places/mutations.ts
+++ src/store/places/mutations.ts
@@ -1,8 +1,12 @@
 import { MutationTree } from "vuex";
 import { PlacesState } from "./state";
 
-const mutations: MutationTree<PlacesState> = {
-
+const mutation: MutationTree<PlacesState> = {
+    setLngLat(state: PlacesState, coords: {lng: number, lat: number}) {
+        console.log({ coords })
+        state.userLocation = [coords.lng, coords.lat]
+        state.isLoading = false
+    }
 }
 
-export default mutations;
+export default mutation;
